46003b08e158e50d91fe553ae37387cab50e1d88
pedrohenriquepaixao <pedrohenriquepaixaoc@gmail.com>
2024-10-08 13:11:47 -0300

    feat: Criando service e interface

---
+++ DevDesafioMagvaBusZap/DevDesafioMagvaBusZap/Service/ApiService.cs
@@ -0,0 +1,63 @@
+﻿using DevDesafioMagvaBusZap.Models;
+using DevDesafioMagvaBusZap.Models.Results;
+using DevDesafioMagvaBusZap.Service.Interface;
+using System.Text.Json;
+
+namespace DevDesafioMagvaBusZap.Service
+{
+    public class ApiService : IApiService
+    {
+        private readonly HttpClient _httpClient;
+        private const string EndpointAndParams = "api/character?status=unknown&species=alien";
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Result<List<Character>>> GetDataByRequestedParameters()
+        {
+
+            var jsonResponse = await GetJsonResponse(EndpointAndParams);
+            var characters = DeserializeCharacters(jsonResponse);
+      
+            if (characters.IsFailure || !characters.Value.Any())
+            {
+                return Result.Failure<List<Character>>(new Error("ApiService.GetCharacterAsync", $"Nenhum dado encontrado"));
+            }
+
+            var characterAppearsInMoreThanOneEpisode = characters.Value.Where(character => character.Episode.Count > 1).ToList();
+            return Result.Success(characterAppearsInMoreThanOneEpisode);
+
+        }
+
+        private async Task<string> GetJsonResponse(string endpoint)
+        {
+            var response = await _httpClient.GetAsync(endpoint);
+            if (!response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Erro status code: {response.StatusCode}, detalhes: {jsonResponse}");
+            }
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private Result<List<Character>> DeserializeCharacters(string jsonResponse)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    PropertyNameCaseInsensitive = true
+                };
+                var resultModel = JsonSerializer.Deserialize<ApiResponse>(jsonResponse, options);
+
+                return Result.Success(resultModel?.results ?? new List<Character>()) ;
+            }
+            catch (JsonException ex)
+            {
+                return Result.Failure<List<Character>>(new Error("ApiService.GetCharacterAsync", $"Erro ao desserializar JSON: {ex.Message}"));
+            }
+        }
+    }
+}
---
+++ DevDesafioMagvaBusZap/DevDesafioMagvaBusZap/Service/Interface/IApiService.cs
@@ -0,0 +1,11 @@
+﻿using DevDesafioMagvaBusZap.Models;
+using DevDesafioMagvaBusZap.Models.Results;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DevDesafioMagvaBusZap.Service.Interface
+{
+    public interface IApiService
+    {
+        public Task<Result<List<Character>>> GetDataByRequestedParameters();
+    }
+}
