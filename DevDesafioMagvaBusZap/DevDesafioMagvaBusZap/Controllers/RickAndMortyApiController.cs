afcbbe8a5c9f87fda07143d612239d846d429f9a
pedrohenriquepaixao <pedrohenriquepaixaoc@gmail.com>
2024-10-08 13:21:59 -0300

    feat: Controller e endpoint para retorno da api

---
+++ DevDesafioMagvaBusZap/DevDesafioMagvaBusZap/Controllers/RickAndMortyApiController.cs
@@ -0,0 +1,28 @@
+﻿using DevDesafioMagvaBusZap.Models.Results;
+using DevDesafioMagvaBusZap.Service;
+using DevDesafioMagvaBusZap.Service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevDesafioMagvaBusZap.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RickAndMortyApiController : ControllerBase
+    {
+        private readonly IApiService _apiService;
+        public RickAndMortyApiController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var result = await _apiService.GetDataByRequestedParameters();
+
+            return result.Match<IActionResult>(
+                    onSuccess: () => Ok(result.Value),
+                    onFailure: error => BadRequest(error.Description)
+                );
+        }
+    }
+}
